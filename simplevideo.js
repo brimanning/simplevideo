10dc9c08f5b7b820984ad2a16c45b948541aaaff
Brian Manning <brimanning@gmail.com>
2014-09-15 14:51:24 -0400

    Major refactor of init method

--- simplevideo.js
+++ simplevideo.js
@@ -59,7 +59,219 @@
       'controls',
       'loop'
     ],
-    utils = {};
+    utils = {},
+    setUpVideoElement = function(options, opt) {
+      var video = {};
+      video.paused = true;
+      video.needsFlash = false;
+
+      if (options.target instanceof jQuery) {
+        video.target = options.target;
+      } else if (typeof options.target === 'string') {
+        video.target = $(options.target);
+      }
+
+      if (!video.target.is('video')) {
+        video.target.append('<video class="simplevideo"></video>');
+        video.target = video.target.find('video.simplevideo');
+      } else {
+        video.target.addClass('simplevideo');
+      }
+
+      if (video.target[0].canPlayType('video/mp4') === '') {
+        video.needsFlash = true;
+      }
+
+      video.getCurrentTime = function() {
+        var currentTime;
+        if (video.needsFlash) {
+          currentTime = video.swf.getCurrentTime();
+        } else {
+          currentTime = video.target[0].currentTime;
+        }
+        return currentTime;
+      };
+
+      video.getDuration = function() {
+        var duration;
+        if (video.needsFlash) {
+          duration = video.swf.getDuration();
+        } else {
+          duration = video.target[0].duration;
+        }
+        return duration;
+      };
+
+      video.setCurrentTime = function(time) {
+        if (typeof time === 'string' && time.indexOf('%') > 0) {
+          time = utils.sanitizePercent(time) * video.getDuration();
+        }
+
+        if (video.needsFlash) {
+          video.swf.setCurrentTime(time);
+        } else {
+          video.target[0].currentTime = time;
+        }
+
+        return video.getCurrentTime();
+      };
+
+      video.getVolume = function() {
+        var volume;
+        if (video.needsFlash) {
+          volume = video.swf.getVolume();
+        } else {
+          volume = video.target[0].volume;
+        }
+        return volume;
+      };
+
+      video.setVolume = function(volume) {
+        if (video.needsFlash) {
+          video.swf.setVolume(utils.sanitizePercent(volume));
+        } else {
+          video.target[0].volume = utils.sanitizePercent(volume);
+        }
+
+        return video.getVolume();
+      };
+
+      video.play = function() {
+        if (video.needsFlash) {
+          video.swf.play();
+          video.paused = false;
+        } else {
+          video.target[0].play();
+          video.paused = video.target[0].paused;
+        }
+        utils.ifFunctionExecute(opt.onPlay);
+
+        if (timeInterval === null) {
+          timeInterval = setInterval(function () {
+            if (utils.checkExists(opt.onPlaying) && typeof opt.onPlaying === 'function') {
+              opt.onPlaying(video.getCurrentTime(), video.getDuration());
+            }
+          }, opt.onPlayingInterval);
+        }
+      };
+
+      video.pause = function() {
+        clearInterval(timeInterval);
+        if (video.needsFlash) {
+          video.swf.pause();
+          video.paused = true;
+        } else {
+          video.target[0].pause();
+          video.paused = video.target[0].paused;
+        }
+        utils.ifFunctionExecute(opt.onPause);
+      };
+
+      return video;
+    },
+    timeInterval = null,
+    ended = function(video, opt) {
+      video.paused = true;
+      clearInterval(timeInterval);
+      timeInterval = null;
+      utils.ifFunctionExecute(opt.onEnded);
+
+      if (utils.checkExists(opt.loop) && opt.loop) {
+        video.setTime(0);
+        video.play();
+      }
+    },
+    processOptions = function(video, options, opt) {
+      for (var i = 0, l = optsList.length; i < l; i++) {
+        opt[optsList[i]] = utils.checkExists(options[optsList[i]]) ? options[optsList[i]] : defaultOpts[optsList[i]];
+        if ((!utils.checkExists(opt[optsList[i]]) || !utils.checkExists(options[optsList[i]])) && attrsList.indexOf(optsList[i]) > -1) {
+          opt[optsList[i]] = utils.checkExists(video.target.attr(attrsList[i])) ? video.target.attr(attrsList[i]) : defaultOpts[optsList[i]];
+          //handle case where the attribute doesn't have a value (defaults true)
+          if (opt[optsList[i]] === attrsList[i]) {
+            opt[optsList[i]] = true;
+          }
+        }
+      }
+
+      if (!utils.checkExists(opt.src) && video.target.find('source').length > 0 && utils.checkExists(video.target.find('source').attr('src'))) {
+        opt.src = video.target.find('source').attr('src');
+      }
+
+    },
+    setUpFlashPlayer = function(video, opt) {
+      utils.checkSWFObject(opt, function() {
+        video.swfId = utils.generateGuid();
+        video.target.replaceWith('<div class="simplevideo"><div id="' + video.swfId + '"></div></div>');
+        var flashVars = {},
+          params = {
+            quality: 'high',
+            bgcolor: '#ffffff',
+            allowscriptaccess: 'always',
+            allowfullscreen: 'true',
+            wmode: 'transparent'
+          },
+          attributes = {
+            id: video.swfId,
+            name: video.swfId,
+            align: 'middle'
+          };
+        swfobject.embedSWF(
+          opt.swfUrl,
+          video.swfId,
+          '100%',
+          '100%',
+          //TODO: determine minimum version
+          '11.4.0',
+          opt.playerProductInstallSwfUrl,
+          flashVars,
+          params,
+          attributes,
+          function(e) {
+            video.swf = e.ref;
+
+            w.simpleVideoSwfReady = function () {
+              if (opt.autoplay) {
+                video.play();
+              }
+            };
+
+            w.simpleVideoSwfEnded = function () {
+              //swf has the habit of calling this twice
+              if (!video.paused) {
+                ended(video, opt);
+              }
+            };
+
+            //even though the swf should be ready, it isn't always
+            var inter = setInterval(function () {
+              if (utils.checkExists(video.swf.init)) {
+                clearInterval(inter);
+                video.swf.init(opt.src);
+              }
+            }, 10);
+          }
+        );
+      });
+
+    },
+    setUpHTML5Player = function(video, opt) {
+      video.target.html('<source src="' + opt.src + '" type="video/mp4" />');
+      if (utils.checkExists(opt.poster)) {
+        video.target.attr('poster', opt.poster);
+      }
+      video.target.prop('loop', opt.loop);
+      video.target.prop('autoplay', opt.autoplay);
+      video.target.prop('controls', opt.controls);
+      video.target[0].load();
+
+      if (opt.autoplay) {
+        video.play();
+      }
+
+      video.target.bind('ended', function() {
+        ended(video, opt);
+      });
+    };
 
   w.simplevideo = {};
 
@@ -119,214 +331,16 @@
   };
 
   w.simplevideo.init = function(options) {
-    var video = {},
-      opt = {},
-      timeInterval = null;
-
-    video.paused = true;
-    video.needsFlash = false;
-
-    if (options.target instanceof jQuery) {
-      video.target = options.target;
-    } else if (typeof options.target === 'string') {
-      video.target = $(options.target);
-    }
-
-    if (!video.target.is('video')) {
-      video.target.append('<video class="simplevideo"></video>');
-      video.target = video.target.find('video.simplevideo');
-    } else {
-      video.target.addClass('simplevideo');
-    }
-
-    if (video.target[0].canPlayType('video/mp4') === '') {
-      video.needsFlash = true;
-    }
-
-    video.getCurrentTime = function() {
-      var currentTime;
-      if (video.needsFlash) {
-        currentTime = video.swf.getCurrentTime();
-      } else {
-        currentTime = video.target[0].currentTime;
-      }
-      return currentTime;
-    };
-
-    video.getDuration = function() {
-      var duration;
-      if (video.needsFlash) {
-        duration = video.swf.getDuration();
-      } else {
-        duration = video.target[0].duration;
-      }
-      return duration;
-    };
-
-    video.setCurrentTime = function(time) {
-      if (typeof time === 'string' && time.indexOf('%') > 0) {
-        time = utils.sanitizePercent(time) * video.getDuration();
-      }
-
-      if (video.needsFlash) {
-        video.swf.setCurrentTime(time);
-      } else {
-        video.target[0].currentTime = time;
-      }
-
-      return video.getCurrentTime();
-    };
-
-    video.getVolume = function() {
-      var volume;
-      if (video.needsFlash) {
-        volume = video.swf.getVolume();
-      } else {
-        volume = video.target[0].volume;
-      }
-      return volume;
-    };
-
-    video.setVolume = function(volume) {
-      if (video.needsFlash) {
-        video.swf.setVolume(utils.sanitizePercent(volume));
-      } else {
-        video.target[0].volume = utils.sanitizePercent(volume);
-      }
-
-      return video.getVolume();
-    };
-
-    video.play = function() {
-      if (video.needsFlash) {
-        video.swf.play();
-        video.paused = false;
-      } else {
-        video.target[0].play();
-        video.paused = video.target[0].paused;
-      }
-      utils.ifFunctionExecute(opt.onPlay);
-
-      if (timeInterval === null) {
-        timeInterval = setInterval(function () {
-          if (utils.checkExists(opt.onPlaying) && typeof opt.onPlaying === 'function') {
-            opt.onPlaying(video.getCurrentTime(), video.getDuration());
-          }
-        }, opt.onPlayingInterval);
-      }
-    };
-
-    video.pause = function() {
-      clearInterval(timeInterval);
-      if (video.needsFlash) {
-        video.swf.pause();
-        video.paused = true;
-      } else {
-        video.target[0].pause();
-        video.paused = video.target[0].paused;
-      }
-      utils.ifFunctionExecute(opt.onPause);
-    };
-
-    var ended = function() {
-      video.paused = true;
-      clearInterval(timeInterval);
-      timeInterval = null;
-      utils.ifFunctionExecute(opt.onEnded);
-
-      if (utils.checkExists(opt.loop) && opt.loop) {
-        video.setTime(0);
-        video.play();
-      }
-    };
+    var opt = {},
+      video = setUpVideoElement(options, opt);
 
     if (utils.checkExists(video.target) && video.target.length > 0) {
-      for (var i = 0, l = optsList.length; i < l; i++) {
-        opt[optsList[i]] = utils.checkExists(options[optsList[i]]) ? options[optsList[i]] : defaultOpts[optsList[i]];
-        if ((!utils.checkExists(opt[optsList[i]]) || !utils.checkExists(options[optsList[i]])) && attrsList.indexOf(optsList[i]) > -1) {
-          opt[optsList[i]] = utils.checkExists(video.target.attr(attrsList[i])) ? video.target.attr(attrsList[i]) : defaultOpts[optsList[i]];
-          //handle case where the attribute doesn't have a value (defaults true)
-          if (opt[optsList[i]] === attrsList[i]) {
-            opt[optsList[i]] = true;
-          }
-        }
-      }
-
-      if (!utils.checkExists(opt.src) && video.target.find('source').length > 0 && utils.checkExists(video.target.find('source').attr('src'))) {
-        opt.src = video.target.find('source').attr('src');
-      }
+      processOptions(video, options, opt);
 
       if (video.needsFlash) {
-
-        utils.checkSWFObject(opt, function() {
-          video.swfId = utils.generateGuid();
-          video.target.replaceWith('<div class="simplevideo"><div id="' + video.swfId + '"></div></div>');
-          var flashVars = {},
-            params = {
-              quality: 'high',
-              bgcolor: '#ffffff',
-              allowscriptaccess: 'always',
-              allowfullscreen: 'true',
-              wmode: 'transparent'
-            },
-            attributes = {
-              id: video.swfId,
-              name: video.swfId,
-              align: 'middle'
-            };
-          swfobject.embedSWF(
-            opt.swfUrl,
-            video.swfId,
-            '100%',
-            '100%',
-            //TODO: determine minimum version
-            '11.4.0',
-            opt.playerProductInstallSwfUrl,
-            flashVars,
-            params,
-            attributes,
-            function(e) {
-              video.swf = e.ref;
-
-              w.simpleVideoSwfReady = function () {
-                if (opt.autoplay) {
-                  video.play();
-                }
-              };
-
-              w.simpleVideoSwfEnded = function () {
-                //swf has the habit of calling this twice
-                if (!video.paused) {
-                  ended();
-                }
-              };
-
-              //even though the swf should be ready, it isn't always
-              var inter = setInterval(function () {
-                if (utils.checkExists(video.swf.init)) {
-                  clearInterval(inter);
-                  video.swf.init(opt.src);
-                }
-              }, 10);
-            }
-          );
-        });
-
+        setUpFlashPlayer(video, opt);
       } else {
-        video.target.html('<source src="' + opt.src + '" type="video/mp4" />');
-        if (utils.checkExists(opt.poster)) {
-          video.target.attr('poster', opt.poster);
-        }
-        video.target.prop('loop', opt.loop);
-        video.target.prop('autoplay', opt.autoplay);
-        video.target.prop('controls', opt.controls);
-        video.target[0].load();
-
-        if (opt.autoplay) {
-          video.play();
-        }
-
-        video.target.bind('ended', ended);
+        setUpHTML5Player(video, opt);
       }
     }
 
